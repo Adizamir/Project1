fabbfc4fb994598fe4affdfc46342492b80cf089
Adizamir <Adi444zamir@gmail.com>
2022-11-13 19:32:53 +0200

    this is

--- Adizamir.java
+++ Adizamir.java
@@ -1,7 +1,7 @@
 public class Adizamir {
      public static void main(String[] args){
 	    for(int i=1; i<=10; i++){
-	 System.out.println("Adi Zamir..."+i);
+	 System.out.println("Adi Zamir. Test1."+i);
 	 }
 	 }
 }
